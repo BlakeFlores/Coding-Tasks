6714c41b9d6b83f5dea2544e9d42d8ec8a4461f8
Blake Flores <blake.i.flores@gmail.com>
2020-10-21 19:00:13 -0700

    Added task2
    Finished task2, now apples are buy-1-get-1 and oranges are buy-2-get-1 free

--- src/Coding_Tasks_From_Tim.kt
+++ src/Coding_Tasks_From_Tim.kt
@@ -1,6 +1,7 @@
 fun main() {
     println("Welcome to the FruitDealer2020")
-    println("Apples: \$.60, Oranges \$.25")
+    println("Apples: \$.60 or Buy 1 get 1 free!\n" +
+            " Oranges \$.25 or Buy 2 get 1 free!")
     println("What can I get for you?")
 
     val userOrder = readLine()
@@ -15,18 +16,18 @@ fun order_parse(userOrder:String){
     var delimiter = " "
     val orderItems = userOrder.split(delimiter)
 
-    var apples = 0.0
-    var oranges = 0.0
+    var apples = 0
+    var oranges = 0
 
     if (orderItems != null){
         for(current_item in orderItems.indices)
         {
             when(orderItems[current_item]){
                 "apple"-> {
-                    apples+=.6
+                    apples++
                 }
                 "orange"->{
-                    oranges+=.25
+                    oranges++
                 }
                 else-> {
                     println("Item $current_item was unable to be read")
@@ -34,6 +35,19 @@ fun order_parse(userOrder:String){
             }
         }
     }
-    println("Total: \$ ${apples+oranges}")
+    val total = special_deals(apples, oranges)
+    println("Total: \$ ${total}")
+
+}
+
+fun special_deals(apples:Int, oranges:Int):Double{
+    val special_apples = apples/2 * .6 //apples where the deal applies
+    val not_special_apples = apples%2 * .6 //apples which are full price
+
+    val special_oranges = oranges/3 *.50 //oranges where the deal applies (co
+    val not_special_oranges = oranges%3 *.25 //oranges which are full price
+
+    return special_apples + not_special_apples + special_oranges + not_special_oranges
+
 
 }
